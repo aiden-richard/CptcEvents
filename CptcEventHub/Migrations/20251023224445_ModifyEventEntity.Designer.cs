1ae0d6bb38811aa4e96613c367c047d618176e5e
aiden-richard <richardaiden2006@gmail.com>
2025-10-23 15:45:42 -0700

    Run Migrations

--- CptcEventHub/Migrations/20251023224445_ModifyEventEntity.Designer.cs
+++ CptcEventHub/Migrations/20251023224445_ModifyEventEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CptcEventHub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CptcEventHub.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251023224445_ModifyEventEntity")]
+    partial class ModifyEventEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0-rc.2.25502.107");
