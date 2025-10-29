1822f1135b23b5aef8132083307a671e0a62b602
Aiden <richardaiden2006@gmail.com>
2025-10-29 14:46:06 -0700

    Remove uneeded stuff from Home controller

--- CptcEventHub/Controllers/HomeController.cs
+++ CptcEventHub/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 using CptcEventHub.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using CptcEvents.Services;
 
 namespace CptcEventHub.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IEventService _eventsService;
-
-        public HomeController(IEventService eventsService)
-        {
-            _eventsService = eventsService;
-        }
-
         public async Task<IActionResult> Index()
         {
             return View();
