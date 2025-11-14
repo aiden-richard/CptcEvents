7effc8741d93958deb39f44b77a18ae478a7d88d
Aiden <richardaiden2006@gmail.com>
2025-11-13 21:56:01 -0800

    Require account confirmation before sign-in

--- CptcEvents/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CptcEvents/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -152,6 +152,9 @@ namespace CptcEvents.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
+                    // Ensure account confirmation is required before sign-in
+                    _userManager.Options.SignIn.RequireConfirmedAccount = true;
+
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
