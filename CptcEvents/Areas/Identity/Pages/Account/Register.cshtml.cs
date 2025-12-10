3d7c46aed40028fc0a6cff81d7fb6be95da61353
Aiden Richard <richardaiden2006@gmail.com>
2025-12-10 08:02:13 -0800

    Add validation attributes for user properties in registration model

--- CptcEvents/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CptcEvents/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -86,6 +86,7 @@ namespace CptcEvents.Areas.Identity.Pages.Account
 
             [Required]
             [Display(Name = "Username")]
+            [StringLength(50)]
             public string Username { get; set; }
 
             /// <summary>
@@ -105,8 +106,8 @@ namespace CptcEvents.Areas.Identity.Pages.Account
             public string LastName { get; set; }
 
             /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
+            /// This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            /// directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -115,8 +116,8 @@ namespace CptcEvents.Areas.Identity.Pages.Account
             public string Password { get; set; }
 
             /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
+            /// This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            /// directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
@@ -127,6 +128,7 @@ namespace CptcEvents.Areas.Identity.Pages.Account
             /// Gets or sets the instructor code.
             /// </summary>
             [Display(Name = "Instructor Code")]
+            [StringLength(8, MinimumLength = 8, ErrorMessage = "The instructor code must be exactly 8 characters long.")]
             public string InstructorCode { get; set; }
         }
 
