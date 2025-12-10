2acd7a0964384ab7f862c4dcfa4d31e3ea64e390
Aiden Richard <richardaiden2006@gmail.com>
2025-12-10 00:08:31 -0800

    docs: Enhance XML documentation for HomeController methods

--- CptcEvents/Controllers/HomeController.cs
+++ CptcEvents/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 
 namespace CptcEvents.Controllers
 {
+    /// <summary>
+    /// Controller for displaying public-facing home and informational pages.
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly IEventService _eventService;
@@ -17,16 +20,31 @@ namespace CptcEvents.Controllers
             _userManager = userManager;
         }
 
+        /// <summary>
+        /// Displays the home page with approved public events.
+        /// GET /
+        /// </summary>
+        /// <returns>Home page view.</returns>
         public async Task<IActionResult> Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Displays the privacy policy page.
+        /// GET /Home/Privacy
+        /// </summary>
+        /// <returns>Privacy policy view.</returns>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Displays the error page when an exception occurs.
+        /// GET /Home/Error
+        /// </summary>
+        /// <returns>Error view with request ID.</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
